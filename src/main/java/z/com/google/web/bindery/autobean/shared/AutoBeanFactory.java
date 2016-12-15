1c0bd4b3a20bba3352826ffbd1794536b1bd6c5d
Marcos Zolnowski <marcos.zolnowski@gmail.com>
2016-12-15 01:06:53 -0200

    First commit

---
+++ src/main/java/z/com/google/web/bindery/autobean/shared/AutoBeanFactory.java
@@ -0,0 +1,9 @@
+package z.com.google.web.bindery.autobean.shared;
+
+/**
+ *
+ * @author Marcos Zolnowski
+ */
+public interface AutoBeanFactory extends com.google.web.bindery.autobean.shared.AutoBeanFactory {
+
+}
